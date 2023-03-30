4332058bba9dff5985cef449907968f08989b8be
Carlos Montoro Navas <Carlosmon2004@gmail.com>
2023-03-30 18:26:21 +0200

    Agregar archivos de proyecto.

---
+++ BGCuentas/Cuenta.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGCuentas
+{
+    // EXCEPCIONES PERSONALIZADAS - PUBLICAS
+    public class TitularException : Exception
+    {
+        public TitularException() : base("Error: Titular Incorrecto") { }
+        public TitularException(string mensage) : base(mensage) { }
+    }
+
+    public class CuentaSinSaldoException : Exception 
+    {
+        public CuentaSinSaldoException() : base("Error: Cuenta sin saldo") { }
+        public CuentaSinSaldoException(string mensage) : base(mensage) { }
+    }
+    public class CantidadException : Exception
+    {
+        public CantidadException() : base("Error: Cuenta sin saldo") { }
+        public CantidadException(string mensage) : base(mensage) { }
+    }
+    public class Cuenta
+    {
+        // CONSTANTES PUBLICO / PRIVADAS
+        private const int LONGITUD_MIN = 5;
+        private const int LONGITUD_MAX = 50;
+        private const double SALDO_INI = 0;
+
+        // MIEMBROS PRIVADOS
+        protected string _titular;
+        protected double _saldo;
+
+        // CONSTRUCTORES
+        public Cuenta(string nombre)
+        {
+            Titular = nombre;                                                                                                                                                                   
+            _saldo = SALDO_INI;
+        }
+
+        public Cuenta(string nombre, double cantidad) : this (nombre)
+        {
+            Ingresar(cantidad);
+        }
+
+        // PROPIEDADES
+        public string Titular
+        {
+            get { return _titular; }
+            set 
+            {
+                // Establecimiento de Valor por defecto
+
+                // Validacion
+                if ((value.Length < LONGITUD_MIN) || (value.Length > LONGITUD_MAX))
+                    throw new TitularException();
+
+                _titular = value;
+            }
+        }
+
+        public double Saldo
+        {
+            get 
+            {
+                // Validacion
+                if (_saldo == SALDO_INI)
+                    throw new CuentaSinSaldoException();
+                return _saldo; 
+            }
+        }
+
+        // METODOS PUBLICOS
+        public void Ingresar (double cantidad)
+        {
+            // Validar la Cantidad
+            try
+            {
+                ValidarCantidad(cantidad);
+            }
+            catch(CantidadException error)
+            {
+                throw new CantidadException("Cantidad a Ingresar Incorrecta");
+            }
+
+            _saldo += cantidad;
+        }
+
+        public virtual void Retirar (double cantidad)
+        {
+            // Validar la Cantidad
+            try
+            {
+                ValidarCantidad(cantidad);
+            }
+            catch (CantidadException error)
+            {
+                throw new CantidadException("Cantidad a Retirar Incorrecta");
+            }
+
+            // Comprobar si hay saldo suficiente
+            if (cantidad > Saldo)
+                throw new CantidadException("No hay suficiente saldo");
+
+            _saldo -= cantidad;
+        }
+
+        // METODOS PRIVADOS
+        private void ValidarCantidad(double valor)
+        {
+            if(valor < SALDO_INI)
+                throw new CantidadException();
+        }
+
+    }
+}
---
+++ BGCuentas/CuentaJoven.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGCuentas
+{
+    public class EdadException : Exception
+    {
+        public EdadException() : base("Edad Incorrecta") { }
+        public EdadException(string mensage) : base(mensage) { }
+    }
+    public class CuentaJoven : Cuenta
+    {
+        // CONSTANTES
+        private const double RETIRAR_LIM = 500;
+        private const int EDAD_MIN = 10;
+        private const int EDAD_MAX = 26;
+
+        // MIEMBROS PRIVADOS
+        private int _edad;
+
+        // CONSTRUCTORES
+        public CuentaJoven (string nombre, int anios) : base(nombre)
+        {
+            Edad = anios;
+        }
+
+        public CuentaJoven (string nombre, double cantidad, int anios) : base(nombre, cantidad)
+        {
+            Edad = anios;
+        }
+
+        // PROPIEDADES
+        public int Edad
+        {
+            get
+            {
+                return _edad;
+            }
+            set
+            {
+                // Validacion del value
+                if((value < EDAD_MIN) || (value > EDAD_MAX))
+                _edad = value;
+            }
+        }
+
+        // METODOS PUBLICOS
+        public override void Retirar(double cantidad)
+        {
+            // Validar la Cantidad
+            if (cantidad > RETIRAR_LIM)
+                throw new CantidadException($"Este tipo de cuenta no permite retiradas de " +
+                    $"cantidades superiores a {RETIRAR_LIM} Euros");
+
+            base.Retirar(cantidad);
+        }
+
+        // METODOS PRIVADOS
+
+    }
+}
